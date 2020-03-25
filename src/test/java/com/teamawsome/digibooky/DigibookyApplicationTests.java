5a1f4312850f4bd31617e59235f6fc43b27c41da
Dries Bodaer <dries.bodaer@cegeka.com>
2020-03-25 11:29:23 +0100

    addedintelij

---
+++ src/test/java/com/teamawsome/digibooky/DigibookyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamawsome.digibooky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigibookyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
