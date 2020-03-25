1c97955f0dab9042df595a01fd9a02e8a28405f8
Dries Bodaer <dries.bodaer@cegeka.com>
2020-03-25 16:14:15 +0100

    first day

--- api/src/main/java/com/teamawsome/DigibookyApplication.java
+++ api/src/main/java/com/teamawsome/DigibookyApplication.java
@@ -1,4 +1,4 @@
-package com.teamawsome.digibooky;
+package com.teamawsome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
