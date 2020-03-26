83ed7595b4590c91512d2ffb427577030bb34357
TheRealTomDC <tom.decrock@vutg.be>
2020-03-26 15:28:37 +0100

    story 6 in progress

--- api/src/main/java/com/teamawsome/api/member/MemberDto.java
+++ api/src/main/java/com/teamawsome/api/member/MemberDto.java
@@ -1,4 +1,27 @@
 package com.teamawsome.api.member;
 
+import com.teamawsome.domain.member.Member;
+
 public class MemberDto {
+    private  int id;
+    private  int inss;
+    private  String eMail;
+    private  String firstName;
+    private  String lastName;
+    private String streetName;
+    private int houseNumber;
+    private int postalCode;
+    private String city;
+
+    public MemberDto(Member member) {
+        this.id = id;
+        this.inss = inss;
+        this.eMail = eMail;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.streetName = streetName;
+        this.houseNumber = houseNumber;
+        this.postalCode = postalCode;
+        this.city = city;
+    }
 }
