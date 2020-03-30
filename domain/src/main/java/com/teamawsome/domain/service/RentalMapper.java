c64c97bd7aea99edabab511782159dad2d87a0cc
Harold Brinkhof <harold.brinkhof@economie.fgov.be>
2020-03-30 21:48:24 +0200

    added tests for RentalMapper & BookMapper

--- domain/src/main/java/com/teamawsome/domain/service/RentalMapper.java
+++ domain/src/main/java/com/teamawsome/domain/service/RentalMapper.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class RentalMapper {
-
     public  LibrarianRentalDto toLibrarianRentalDto(Rental rental){
         return new LibrarianRentalDto(rental.getRentalId(), rental.getBook(), rental.getMember(),
                 rental.getStartingDate(), rental.getReturnDate());
