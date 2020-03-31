bc5f140b952f2505f919c18ae11c7adc867f665d
Dries Bodaer <dries.bodaer@cegeka.com>
2020-03-31 11:47:53 +0200

    thuesday, added logging

--- api/src/main/java/com/teamawsome/api/book/BookController.java
+++ api/src/main/java/com/teamawsome/api/book/BookController.java
@@ -10,6 +10,9 @@ import com.teamawsome.domain.dto.DetailedBookDto;
 import com.teamawsome.domain.service.BookMapper;
 import com.teamawsome.domain.dto.FindByAuthorDto;
 import com.teamawsome.domain.service.LibraryManagement;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -23,6 +26,7 @@ public class BookController {
     private final BookRepository bookRepository;
     private final BookMapper bookMapper;
     private final LibraryManagement libraryManagement;
+
 
     @Autowired
     public BookController(BookRepository bookRepository, BookMapper bookMapper, LibraryManagement libraryManagement) {
